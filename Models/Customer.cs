d5ce0aa1718c7eb73e68a1b39f9ce6a3fcc42c8b
samoski615 <samoski615@gmail.com>
2019-09-05 14:31:33 -0500

    had to re-create my _LayoutCustomer.cshtml file due to error with it not saving; cannot get Delete Customer method working due to unknown error--Mike T. said not to worry about getting it working as it's not in user stories; I'll research the 'aborted action' error later but for now Delete profile will not be a thing that a customer can do

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,6 +11,7 @@ namespace TrashCollector.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double MonthlyBalance { get; set; }
